155524ebecd52d353c68dbb0cc2fec5c34c20f74
kjameslubin <kieren1@gmail.com>
2015-10-30 02:06:44 -0700

    added password route

--- server-task/app.js
+++ server-task/app.js
@@ -6,23 +6,35 @@ var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require("fs"));
 
 var blocRootDir = path.normalize(path.join(__dirname, '..'));
+var yamlConfig = require(path.join(blocRootDir, 'lib', 'yaml-config.js'));
+var configFile = yamlConfig.readYaml('config.yaml');
+var bodyParser = require('body-parser');
 
 app.engine('.hbs', exphbs({defaultLayout: 'SampleLayout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use('/css', express.static('css'));
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+var globalPassword = '';
 
 function contractLookup(contractName) {
     var contractNameSol = contractName + '.sol';
     return fs.statAsync(path.join(__dirname, 'contracts/'+contractNameSol)).then(
          function (res) {
              console.log("contract found, checking if it has been compiled next"); 
+              
              return { contractExists : true, 
                       contractName : contractName,
-                      contractNameSol : contractNameSol }; 
+                      contractNameSol : contractNameSol,
+                      serverURI : configFile.apiURL }; 
          },
          function (err) { 
              console.log("contract not found, err: " + err); 
@@ -30,7 +42,8 @@ function contractLookup(contractName) {
                       JSON.stringify(
                         {  contractExists : false, 
                            contractName : contractName, 
-                           contractNameSol : contractNameSol }
+                           contractNameSol : contractNameSol,
+                           serverURI : configFile.apiURL }
                       )
                    ); 
           } 
@@ -45,7 +58,21 @@ function contractJSONLookup(contractObj) {
              console.log("contract has been compiled"); 
              contractObj.contractIsCompiled = true;
              contractObj.contractData = JSON.parse(fileData);
-            
+             contractObj.contractDataString = fileData;
+
+             var symtab = contractObj.contractData.symTab;
+
+             var funcs = Object.keys(symtab).filter(function(name) {
+                return symtab[name]["jsType"] === "Function";
+             });
+
+             contractObj.funcs = funcs.map(function(funcName) {
+                 return {
+                     name: funcName,
+                     args: symtab[funcName].functionArgs.map(function(arg){
+                           return {argName: arg};
+                    })};
+             });
              return contractObj;
         }, 
         function (err) { 
@@ -61,8 +88,13 @@ function keyJSONLookup(contractObj) {
         function(fileData) {
             console.log("key present"); 
             contractObj.hasKey = true;
-            contractObj.developerKey = JSON.parse(fileData);
+            contractObj.developerKeystore = JSON.parse(fileData);
+            contractObj.globalKeystore = contractObj.developerKeystore;
+            contractObj.walletAddress = contractObj.developerKeystore.addresses[0];
             
+            contractObj.developerKeystoreString = fileData;
+            contractObj.globalKeystoreString = fileData;
+
             return contractObj;
         }, 
         function (err) { 
@@ -73,11 +105,16 @@ function keyJSONLookup(contractObj) {
 
     );
 }
+
+app.post('/login', function (req, res) {
+  globalPassword = req.body.password;
+  res.send("set your wallet password, thanks");
+});
  
 app.get('/contracts/:contractName', function (req, res) {
   var contractName = req.params.contractName;
   var contractNameSol = contractName + '.sol';
-
+  
   contractLookup(contractName)
    .then(
         function (contractTemplateObj) {
@@ -89,6 +126,7 @@ app.get('/contracts/:contractName', function (req, res) {
         }
     ).then(
         function (contractTemplateObj) {
+          contractTemplateObj.globalPassword = globalPassword;
           res.render('Contract', contractTemplateObj);
         }
     ).catch(function(err) {
@@ -96,8 +134,6 @@ app.get('/contracts/:contractName', function (req, res) {
           res.render('Contract', JSON.parse(err.message));
         }
     );
-  
-
 });
 
   var server = app.listen(3000, function () {

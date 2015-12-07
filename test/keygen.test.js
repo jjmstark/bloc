e2aa1bc3d9c1c19c077137ecee0e6d6f7365a29a
Kieren James-Lubin <kieren1@gmail.com>
2015-12-06 22:18:11 -0500

    added failing tests for multiple key generation

--- test/keygen.test.js
+++ test/keygen.test.js
@@ -17,7 +17,6 @@ var fsMock = {
 
 keygen.__set__("fs", fsMock);
 
-	
 describe('Key Generation', function() {
     describe('#generateKeyPreFaucet()', function () {
 	var mockedKey;
@@ -60,6 +59,61 @@ describe('Key Generation', function() {
 
     	it('should POST to the faucet', function () {
 	    return keygen.generateKey(password).then(function(result) {
+	        console.log("result: " + JSON.stringify(result));
+	    }, function (err) {
+		console.log("err: " + JSON.stringify(err));
+	    });
+	});
+   });
+});
+
+describe('Multi Key Generation', function() {
+    describe('#generateKeysPreFaucet()', function () {
+	var mockedKeyArray;
+	var password;
+	var numKeys;
+	
+        before(function() {
+	    password = 'thepassword';
+	    numKeys = 3;
+	    mockedKeyArray = keygen.generateKeysPreFaucet(password,numKeys);
+        });
+
+	it('should create a key ' + numKeys + ' files, each with an address and a privkey', function () {
+            expect(mockedKeyArray.addresses).not.to.be.empty;
+	    expect(mockedKeyArray.encMasterPriv).not.to.be.empty;
+	    expect(mockedKeyArray.encPrivKeys).not.to.be.empty;
+	});
+
+    	it('should successfully encrypt and decrypt each with the right password', function () {
+            var exportedArray = undefined;
+	    
+	    expect(exported).not.to.be.undefined;
+	});
+
+        it('should throw an exception if the password is incorrect', function () {
+            expect(false).to.be(true);
+	});
+    });
+    
+    describe('#generateKeys()', function () {
+	var mockedKeyArray;
+	var mockedKeyFileArray;
+	var password;
+	var numKeys;
+	
+	before(function() {
+            password = 'thepassword';
+	    numKeys = 3;
+	    mockedKeyArray = keygen.generateKeyPreFaucet(password);
+	    mockedKeyFile = keygen.writeKeysToDisk(mockedKeyArray);
+	});
+	it('should create a key files called key.json', function () {
+            expect(mockedKeyFile.path).to.match(/key*.json/);
+	});
+
+    	it('should POST to the faucet', function () {
+	    return keygen.generateKeys(password,numKeys).then(function(result) {
 	        console.log("result: " + JSON.stringify(result));
 	    }, function (err) {
 		console.log("err: " + JSON.stringify(err));
